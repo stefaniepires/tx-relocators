fbbc69148d9d0c604c71793f8a7a666c53dc0470
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-01-19 11:38:49 -0600

    made some updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -30,3 +30,11 @@ function updateCountdown() {
 }
 
 setInterval(updateCountdown, 1000);
+
+
+$(function () {
+  $(document).scroll(function () {
+      var $nav = $("#mainNavbar");
+      $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
