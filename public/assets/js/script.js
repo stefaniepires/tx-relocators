d8321cb36cdc96e1adb4a37ccd597d0e60337027
steftinsley <69828366+steftinsley@users.noreply.github.com>
2021-01-19 11:05:59 -0600

    added countdown functionality

---
+++ public/assets/js/script.js
@@ -0,0 +1,36 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+const countdown = document.getElementById('countdown');
+const year = document.getElementById('year');
+const loading = document.getElementById('loading');
+
+const now = new Date();
+const eventDate = new Date (2021, 06, 15);
+
+
+// Update countdown time
+function updateCountdown() {
+  const currentTime = now.getTime();
+  const eventTime = eventDate.getTime();
+  const diff = eventTime - currentTime;
+
+  const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+  const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+  const m = Math.floor(diff / 1000 / 60) % 60;
+  const s = Math.floor(diff / 1000) % 60;
+
+  // Add values to DOM
+  days.innerHTML = d;
+  hours.innerHTML = h < 10 ? '0' + h : h;
+  minutes.innerHTML = m < 10 ? '0' + m : m;
+  seconds.innerHTML = s < 10 ? '0' + s : s;
+
+
+
+}
+
+
+// Run every second
+setTimeout(updateCountdown, 1000);
